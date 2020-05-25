d7fe32aad1ab13811881ab6049ba4377abb82f7a
Bailey Niemiller <baileyniemiller@gmail.com>
2020-05-24 22:08:20 -0500

    add css

--- client.js
+++ client.js
@@ -6,12 +6,23 @@ let total = 0;
 function readyNow() {
     // when the submit button is clicked, run the addInfo function
     $('#submitButton').on('click', addInfo);
+    $('#submitButton').mouseenter(subMouseEnter);
+    $('#submitButton').mouseleave(subMouseLeave);
+
+    $('input').mouseenter(inMouseEnter);
+    $('input').mouseleave(inMouseLeave);
+
+    $('.delete').mouseenter(delMouseEnter);
+    $('.delete').mouseleave(delMouseLeave);
+    
 }
 
 // start addInfo function 
 function addInfo() {
-    // add a new row
+    
+    // create a new row
     let newRow = $('<tr></tr>');
+    // add the row to the table body
     $('tbody').append(newRow);
     // declare firstName as the value of the first name input
     let firstName = $('#firstName').val();
@@ -51,7 +62,9 @@ function addInfo() {
     total += newTotal;
     $('#totalMonthly').empty();
     $('#totalMonthly').append(parseInt(total));
+    // empty
     $('#annualSalary').val('');
+    // if the monthly total is greater than $20,000, turn background red
     if (total > 20000) {
         $('h3').css('background-color', 'red');
     }
@@ -60,8 +73,10 @@ function addInfo() {
 
     // declare button
     let deleteButton = $(`<button class="delete">Delete</button>`);
+    // add it to each new row
     $(newRow).append(deleteButton);
 
+    // if the delete button is clicked, delete that whole row that the button is in
     $('.delete').on('click', function (event) {
         const element = event.target;
         $(element).parent().remove();
@@ -69,7 +84,32 @@ function addInfo() {
 }
 
 
+// css functions
+function subMouseEnter() {
+    $(this).css('color', '#36c7af');
+    $(this).css('background-color', '#ffffff');
+}
+function subMouseLeave() {
+    $(this).css('color', '#ffffff');
+    $(this).css('background-color', '#000000');
+}
 
+function inMouseEnter() {
+    $(this).css('background-color', '#f5f5f5');
+}
+function inMouseLeave() {
+    $(this).css('background-color', '#ffffff');
+}
+
+//how come this one isn't working?
+function delMouseEnter() {
+    $(this).css('color', '#36c7af');
+    $(this).css('background-color', '#ffffff');
+}
+function delMouseLeave() {
+    $(this).css('color', '#ffffff');
+    $(this).css('background-color', '#000000');
+}
 
 
 
@@ -83,4 +123,4 @@ function addInfo() {
 // and create a new row each time
 
 // we also want the value from salary to be stored somewhere
-// so all the values can be added up for the total monthly
+// so all the values can be added up for the total monthly (total/12)
